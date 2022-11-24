fc91c0da50968552befd1364a38c1ff5e97b5502
Mohd Daniyal <mohddaniyalh@gmail.com>
2022-11-24 13:29:24 +0530

    Project Updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import django_heroku
 import os
 import environ
 
@@ -125,3 +126,5 @@ LOGIN_URL = "login"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
+django_heroku.settings(locals())
